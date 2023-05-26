a15599caeb1bcb670e3b1eb097d03fa7b66fcea9
Tejal Salve <60121669+tejalsalve19@users.noreply.github.com>
2023-05-26 17:34:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,10 +7,10 @@ const person = {
 
 // Write your code here, make sure the name of variables is exactly same as mentioned in the problem statement
 
-const team = [...players];
-
+const team = [players];
+const team1=[...players];
 // Copying the object
-const cap1 = {...person};
+const cap1 = person;
 
 // Logging the results
 
